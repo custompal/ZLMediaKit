a73514a7184296137d70aa54acf73f72341ddcbf
custompal <1040801756@qq.com>
2022-02-10 11:17:26 +0800

    临时还原修改

--- src/Rtp/PSDecoder.cpp
+++ src/Rtp/PSDecoder.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) 2016 The ZLMediaKit project authors. All Rights Reserved.
  *
  * This file is part of ZLMediaKit(https://github.com/xia-chu/ZLMediaKit).
@@ -9,18 +9,13 @@
  */
 
 #if defined(ENABLE_RTPPROXY)
+
 #include "PSDecoder.h"
 #include "mpeg-ps.h"
-#include "mpeg-ts-proto.h"
-namespace mediakit{
 
-static inline bool isAudio(int codecid) {
-    //暂时只碰到G711A/U的音频时钟频率不统一问题，其他AAC和Opus待测试，按正常处理
-    if (codecid == PSI_STREAM_AUDIO_G711A || codecid == PSI_STREAM_AUDIO_G711U) {
-        return true;
-    }
-    return false;
-}
+using namespace toolkit;
+
+namespace mediakit{
 
 PSDecoder::PSDecoder() {
     _ps_demuxer = ps_demuxer_create([](void* param,
@@ -32,13 +27,7 @@ PSDecoder::PSDecoder() {
                                        const void* data,
                                        size_t bytes){
         PSDecoder *thiz = (PSDecoder *)param;
-        if (thiz->_on_decode) {
-            //DebugL << "codec id: " << codecid << ", pts: " << pts << ", dts: " << dts;
-            if (isAudio(codecid)) {
-                if (0 == thiz->_audio_clock_rate)
-                    thiz->guessAudioClockRate(codecid, dts);
-                thiz->modifyAudioTimestamp(pts, dts);
-            }
+        if(thiz->_on_decode){
             thiz->_on_decode(stream, codecid, flags, pts, dts, data, bytes);
         }
         return 0;
@@ -88,33 +77,6 @@ const char *PSDecoder::onSearchPacketTail(const char *data, size_t len) {
               << ", hex=" << hexdump(data, MIN(len, 32));
         //触发断言，解析失败，丢弃所有数据
         return data + len;
-    }
-}
-
-void PSDecoder::guessAudioClockRate(int codecid, int64_t dts) {
-    if (_first_audio_frame) {
-        _first_audio_frame = false;
-        _last_audio_dts = dts;
-    } else {
-        do {
-            DebugL << "audio codec id: " << codecid << ", dts: " << dts << ", last dts: " << _last_audio_dts;
-            if (dts > _last_audio_dts && dts - _last_audio_dts < 10 * 90) { // 假定音频每帧间隔大于 10ms，如果是标准流，后一帧与前一帧音频时间戳之差必大于 10 * 90
-                if (codecid == PSI_STREAM_AUDIO_G711A || codecid == PSI_STREAM_AUDIO_G711U) { // 暂时只处理G711A/U
-                    _audio_clock_rate = 8;
-                    break;
-                }
-            }
-            _audio_clock_rate = 90;
-        } while (0);
-
-        DebugL << "audio codec id: " << codecid << ", clock rate: " << _audio_clock_rate;
-    }
-}
-
-void PSDecoder::modifyAudioTimestamp(int64_t &pts, int64_t &dts) {
-    if (_audio_clock_rate != 0 && _audio_clock_rate != 90) {
-        dts = (dts / _audio_clock_rate) * 90;
-        pts = dts;
     }
 }
 
